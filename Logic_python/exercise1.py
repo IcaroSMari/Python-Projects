51f6764a9343259bde73cbe9c2884b7982ce523b
Icaro Mari <icarosmprofissional@gmail.com>
2026-03-19 18:20:35 -0300

    Create exercise1.py

---
+++ Logic_python/exercise1.py
@@ -0,0 +1,13 @@
+nome = "Ícaro"
+sobrenome = "Souza"
+idade = 20
+ano_de_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+print(asdasd)
+print()
+print()
+print()
+print()
+print()
